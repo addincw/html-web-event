f4f371c7e5639159a5ca6bad5172bc659b4ff322
addincw <rockydocky007@yahoo.com>
2019-01-10 20:05:22 +0700

    new detail event

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,15 @@ const browserSync = require('browser-sync').create()
 
 sass.compiler = require('node-sass')
 
-browserSync.init({
-  server: "./src"
-});
+// browserSync.init({
+//   server: "./src"
+// });
 
 function css(){
   return src('./src/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(dest('./src/css'))
-    .pipe(browserSync.stream());
+    // .pipe(browserSync.stream());
 }
 
 function js(){
@@ -27,5 +27,5 @@ function fas(){
 }
 
 watch('./src/scss/*.scss', css)
-watch('./src/*.html').on('change', browserSync.reload)
+// watch('./src/*.html').on('change', browserSync.reload)
 exports.default = parallel(fas, css, js)
